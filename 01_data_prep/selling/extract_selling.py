3d698e5f52451ea34bb6f7008c328a89cc3fdb25
BismaBRJ <bismabrj@gmail.com>
2025-08-17 23:57:58 +0700

    Made price tags more uniform/standardized: in case the initial take-all-strings-not-tags step actually results in some strings, store each parent tag instead. Good thing they are NavigableString's and not just str's

--- 01_data_prep/selling/extract_selling.py
+++ 01_data_prep/selling/extract_selling.py
@@ -20,27 +20,31 @@ soup = BeautifulSoup(html_text, "html.parser")
 # [...] Rp [???].[?]00 [...]
 contains_price_re = re.compile("(.*?)Rp([0-9.,\s]*)00(.*)")
 
-price_tags_raw = soup.find_all("div", string=contains_price_re)
+price_descs_raw = soup.find_all("div", string=contains_price_re)
 
-price_tags_navstr = [ # strings only
-    tag
-    for tag in price_tags_raw
-    if (type(tag) == NavigableString)
+# only taking the strings (not tags containing them) to prevent duplication
+price_navstrs = [
+    descendant
+    for descendant in price_descs_raw
+    if (type(descendant) == NavigableString)
 ]
 
-# possibly it is not the strings themselves that are found,
+# possibly it is not the strings themselves that were found,
 # rather the tags containing the strings
-if len(price_tags_navstr) == 0:
+if len(price_navstrs) == 0:
     price_tags = [ # doesn't have yet another div inside
         tag
-        for tag in price_tags_raw
+        for tag in price_descs_raw
         if
             (len(tag.contents) == 1) and
             (type(tag.contents[0]) == NavigableString)
     ]
 else: # in case the strings themselves were found already
-    price_tags = price_tags_navstr
-    # shallow copy is fine, just renaming variables anyway
+    # for uniformity, store the parent tags instead of the navstrings
+    price_tags = [
+        navstr.parent
+        for navstr in price_navstrs
+    ]
 
 def debug_price_tags():
     print("print(len(price_tags)):")
@@ -63,6 +67,6 @@ def debug_price_tags():
     print(price_tags[0].parent.parent)
     print("=" * 10)
 
-debug_price_tags()
+#debug_price_tags()
 
 # to be done

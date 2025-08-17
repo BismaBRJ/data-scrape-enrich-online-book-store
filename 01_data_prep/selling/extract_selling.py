de7d215ec98eaa93b6ae444ec0fdbe18496052e4
BismaBRJ <bismabrj@gmail.com>
2025-08-17 23:47:03 +0700

    Got all thirty price tags. The code also considers the possibility of obtaining NavigableString's (instead of tags) from find_all

--- 01_data_prep/selling/extract_selling.py
+++ 01_data_prep/selling/extract_selling.py
@@ -1,5 +1,7 @@
 # Imports
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
+import re
+import pandas as pd
 
 # Constants (paths etc)
 SELLING_HTML_PATH = (
@@ -14,5 +16,53 @@ with open(SELLING_HTML_PATH, "r") as file:
     html_text = file.read()
 
 soup = BeautifulSoup(html_text, "html.parser")
+
+# [...] Rp [???].[?]00 [...]
+contains_price_re = re.compile("(.*?)Rp([0-9.,\s]*)00(.*)")
+
+price_tags_raw = soup.find_all("div", string=contains_price_re)
+
+price_tags_navstr = [ # strings only
+    tag
+    for tag in price_tags_raw
+    if (type(tag) == NavigableString)
+]
+
+# possibly it is not the strings themselves that are found,
+# rather the tags containing the strings
+if len(price_tags_navstr) == 0:
+    price_tags = [ # doesn't have yet another div inside
+        tag
+        for tag in price_tags_raw
+        if
+            (len(tag.contents) == 1) and
+            (type(tag.contents[0]) == NavigableString)
+    ]
+else: # in case the strings themselves were found already
+    price_tags = price_tags_navstr
+    # shallow copy is fine, just renaming variables anyway
+
+def debug_price_tags():
+    print("print(len(price_tags)):")
+    print(len(price_tags))
+    print("=" * 10)
+
+    print("print(price_tags):")
+    print(price_tags)
+    print("=" * 10)
+
+    print("print(print(price_tags[0])):")
+    print(price_tags[0])
+    print("=" * 10)
+
+    print("print(price_tags[0].parent):")
+    print(price_tags[0].parent)
+    print("=" * 10)
+
+    print("print(price_tags[0].parent.parent):")
+    print(price_tags[0].parent.parent)
+    print("=" * 10)
+
+debug_price_tags()
 
 # to be done

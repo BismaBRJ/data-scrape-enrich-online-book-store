6498c3a8ed290660142d58cb02a363b0a95a7dce
BismaBRJ <bismabrj@gmail.com>
2025-09-02 12:25:56 +0700

    migrate rename keyword code from using os library to using pathlib

--- 01_dataset_from_html/rename_keyword.py
+++ 01_dataset_from_html/rename_keyword.py
@@ -1,28 +1,28 @@
 # This script renames all files in a folder to replace a certain keyword
 
 # Imports
-import os
+from pathlib import Path
 import re
 
 # Constants
-FOLDER_PATH = "01_data_prep/shop_html"
-OLD_KEYWORD = "(store name)" # was once the store name
+FOLDER_PATH = Path(__file__).parent / "shop_html"
+OLD_KEYWORD = "Store Name" # was once the store name
 NEW_KEYWORD = ""
 
 # Script
 
-all_files_folders = os.listdir(FOLDER_PATH)
-old_files = [
-    x
-    for x in all_files_folders
-    if os.path.isfile(os.path.join(FOLDER_PATH, x))
+all_files_folders = FOLDER_PATH.iterdir()
+old_names = [
+    old_path.name
+    for old_path in all_files_folders
+    if old_path.is_file()
 ]
 
 old_pattern = re.compile(OLD_KEYWORD)
 
-for old_name in old_files:
+for old_name in old_names:
     new_name = re.sub(old_pattern, NEW_KEYWORD, old_name)
 
-    old_path = os.path.join(FOLDER_PATH, old_name)
-    new_path = os.path.join(FOLDER_PATH, new_name)
-    os.rename(old_path, new_path)
+    old_path = FOLDER_PATH / old_name
+    new_path = FOLDER_PATH / new_name
+    old_path.rename(new_path)

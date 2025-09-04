96c7e9a5d6dac271e0e5e5e4cfbc74aab85c2655
BismaBRJ <bismabrj@gmail.com>
2025-09-05 00:01:04 +0700

    so far used scikit-learn's built-in precision-recall plot, and boy is it very ugly LOL I might just resort to... I might just try SVM next lol

---
+++ 02_enrich_data/image_similarity_classification/plot_prec_recall.py
@@ -0,0 +1,40 @@
+"""
+Plot Precision-Recall from .npy files
+"""
+
+# Imports
+print("Importing packages...")
+from pathlib import Path
+import numpy as np
+from sklearn.metrics import PrecisionRecallDisplay
+import matplotlib.pyplot as plt
+print("Imports done")
+
+# Constants (settings, paths etc)
+NPY_FOLDER_PATH = (
+    Path(__file__).parent / "embeddings_etc"
+)
+POS_TRUTH_NPY_NAME = "all_pos_sim_scores" # with or without .npy
+NEG_TRUTH_NPY_NAME = "all_neg_sim_scores"
+
+# Script
+
+print("Opening files at", NPY_FOLDER_PATH)
+pos_path = (NPY_FOLDER_PATH / POS_TRUTH_NPY_NAME).with_suffix(".npy")
+neg_path = (NPY_FOLDER_PATH / NEG_TRUTH_NPY_NAME).with_suffix(".npy")
+pos_arr = np.load(pos_path)
+neg_arr = np.load(neg_path)
+print(".npy files opened")
+
+pos_n = len(pos_arr)
+neg_n = len(neg_arr)
+
+all_arr = np.hstack((pos_arr, neg_arr))
+truth_arr = np.hstack((np.ones(pos_n), np.zeros(neg_n)))
+
+display = PrecisionRecallDisplay.from_predictions(
+    truth_arr, all_arr,
+    name="vit_b_32 embeddings", plot_chance_level=True, despine=True
+)
+_ = display.ax_.set_title("2-class Precision-Recall curve")
+plt.show()
